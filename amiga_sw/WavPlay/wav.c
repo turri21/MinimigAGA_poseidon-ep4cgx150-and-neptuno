3194d45d2829db4ad3ce731ff56fa7b5fc766989
Alastair M. Robinson <robinsonb5@gmail.com>
2020-10-21 19:59:03 +0100

    Updated MinimigUtils.adf and WavPlay.

--- amiga_sw/WavPlay/wav.c
+++ amiga_sw/WavPlay/wav.c
@@ -3,6 +3,9 @@
 
 #include "wav.h"
 
+void ErrorMessage(const char *msg);
+
+
 int wav_read(struct Wav *wav,char *buf,int size)
 {
     int result=0;
@@ -43,7 +46,21 @@ struct Wav *wav_open(const char *filename)
                     if(strncmp("fmt ",tmp,4)==0)
                     {
                         printf("Found fmt chunk\n");
+                        if(Read(wav->file,tmp,16)!=16)
+                            ok=0;
+                        l-=16;
+                        if(tmp[0]!=1 || tmp[1]!=0) /* PCM? */
+                            ok=0;
+                        if(tmp[2]!=2 || tmp[3]!=0) /* Stereo */
+                            ok=0;
+                        if(tmp[4]!=0x44 || tmp[5]!=0xac || tmp[6]!=0 || tmp[7]!=0) /* 44100Hz */
+                            ok=0;
+                        if(tmp[14]!=16 || tmp[15]!=0) /* 16 bits */
+                            ok=0;
+                        if(!ok)
+                            ErrorMessage("Format must be 44100Hz 16bit stereo.");
                     }
+
                     else if(strncmp("data",tmp,4)==0)
                     {
                         printf("Found data chunk with length %d\n",l);
@@ -56,9 +73,11 @@ struct Wav *wav_open(const char *filename)
                         Seek(wav->file,l,OFFSET_CURRENT);
                 }
             }
+            else
+                ErrorMessage("Not a WAV file");
         }
         else
-            printf("Can't read header\n");
+            ErrorMessage("Can't read header");
         if(!ok)
         {
             Close(wav->file);
